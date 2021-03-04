7bc65d8953aace5690ebc95f57fd8bfcc0fc74b2
brianyjtai1994 <briantjtai1994@gmail.com>
2021-03-05 06:10:08 +0800

    can be optional-normalized

--- src/spectra/opp.jl
+++ src/spectra/opp.jl
@@ -26,19 +26,22 @@ struct TimeDelay{T} <: AbstractSpectra{T}
     end
 end
 
-struct DiffReflect{N, T, S} <: AbstractSpectra{T}
+struct DiffReflect{N,T,S} <: AbstractSpectra{T}
     ΔR::Matrix{T}; spec::NTuple{N, S}
 
-    DiffReflect(ΔR::Matrix{T}, spec::NTuple{N, S}) where {N, T<:Real, S} = new{N, T, S}(ΔR, spec)
+    DiffReflect(ΔR::Matrix{T}, spec::NTuple{N,S}) where {N,T<:Real,S} = new{N,T,S}(ΔR, spec)
 
-    function DiffReflect(pth::String, spec::NTuple{N, S}) where {N, S}
-        ΔR = readdlm(pth); nrow, ncol = size(ΔR)
+    function DiffReflect(pth::String, spec::NTuple{N,S}; ifnorm::Bool=true) where {N,S}
+        ΔR = readdlm(pth)
 
-        @inbounds begin
-            for j in 1:ncol
-                abs_minΔR = abs(minimum(view(ΔR, :, j)))
-                @simd for i in 1:nrow
-                    ΔR[i, j] /= abs_minΔR
+        if ifnorm
+            nrow, ncol = size(ΔR)
+            @inbounds begin
+                for j in 1:ncol
+                    abs_minΔR = abs(minimum(view(ΔR, :, j)))
+                    @simd for i in 1:nrow
+                        ΔR[i, j] /= abs_minΔR
+                    end
                 end
             end
         end
